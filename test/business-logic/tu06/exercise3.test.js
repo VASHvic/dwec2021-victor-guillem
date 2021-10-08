b79b6865a27bdadd82f12c77aadc7458ee5ae5f3
Victor Guillem <vforvanish@gmail.com>
2021-10-08 18:47:28 +0200

    mock function clase

--- test/business-logic/tu06/exercise3.test.js
+++ test/business-logic/tu06/exercise3.test.js
@@ -1,17 +1,12 @@
 import { pow } from '../../../docs/tu06/exercise3';
-import {
-  ask,
-  isPositive,
-  showPositive,
-  showNegative,
-} from '../../../docs/tu06/exercise4';
+import { ask, showPositive, showNegative } from '../../../docs/tu06/exercise4';
 
 describe('Function pow Test Suite', () => {
   test('Green Path 1', () => {
-    expect(ask(2, 6)).toEqual(64);
+    expect(pow(2, 6)).toEqual(64);
   });
   test('Green Path 2', () => {
-    expect(ask(0, 5)).toEqual(0);
+    expect(pow(0, 5)).toEqual(0);
   });
   test('Green Path 3', () => {
     expect(pow(-5, 3)).toEqual(-125);
@@ -19,10 +14,9 @@ describe('Function pow Test Suite', () => {
 });
 
 describe('Function ask Test Suite', () => {
-  test('Green Path 1', () => {
-    expect(ask(23)).toEqual(64);
-  });
-  test('Green Path 2', () => {
-    expect(ask(-23)).toEqual(0);
+  test('Green Path 1, positive', () => {
+    console.info = jest.fn();
+    ask(23, showPositive, showNegative);
+    expect(console.info).toHaveBeenCalledWith('The number is positive');
   });
 });
