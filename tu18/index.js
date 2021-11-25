fee78e33a2a8032028d2a15aab4367a93f9f7b53
VASHvic <vforvanish@gmail.com>
2021-11-25 20:57:55 +0100

    node elements

---
+++ tu18/index.js
@@ -0,0 +1,6 @@
+const title = document.querySelector("div");
+const list = title.nextElementSibling;
+const secondLi = list.lastElementChild;
+console.log(title);
+console.log(list);
+console.log(secondLi);
