8488be154f3f33d4dc65960fb319c700fd19542b
VASHvic <vforvanish@gmail.com>
2022-01-22 17:13:43 +0100

    promsesa hecho falta refactorizar y mirar si res = ok

--- docs/2nTrimestre/tu30/index.js
+++ docs/2nTrimestre/tu30/index.js
@@ -1,21 +1,31 @@
 const addMoreBtn = document.getElementById("add");
 const retrieveBtn = document.getElementById("retrieve");
 const firstInput = document.querySelector("input");
+const ul = document.createElement("ul");
+document.body.insertBefore(ul, addMoreBtn);
 addMoreBtn.addEventListener("click", createInput);
-retrieveBtn.addEventListener("click", retrieveData);
-const list = document.createElement("ul");
+retrieveBtn.addEventListener("click", fetchAll);
 
 function createInput() {
   const newInput = document.createElement("input");
-
   document.body.append(newInput);
 }
 
-async function retrieveData(name) {
+function printData(name) {
+  let newLi = document.createElement("li");
+  newLi.innerText = name;
+  ul.appendChild(newLi);
+}
+
+async function fetchAll() {
   const allInputs = document.querySelectorAll("input");
-  allInputs.forEach(async (input) => {
-    let result = await fetch(`https://api.github.com/users/${input.value}`);
-    let resultJson = await result.json();
-    list.append(resultJson.login);
+  const promesas = [];
+  allInputs.forEach((input) => {
+    promesas.push(fetch(`https://api.github.com/users/${input.value}`));
+  });
+  const result = await Promise.allSettled(promesas);
+  result.forEach((respuesta) => {
+    respuesta.value.json().then((e) => printData(e.login));
   });
 }
+//refactorizzar y chckear if ok
